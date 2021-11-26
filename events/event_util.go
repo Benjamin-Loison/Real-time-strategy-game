783a35cb2b2911eab5da64f07e09c88691c4ae7f
Gabriel Jeantet <gabriel.jeantetpro@gmail.com>
2021-11-26 15:42:22 +0100

    antoine je te passe la balle

--- events/event_util.go
+++ events/event_util.go
@@ -1,11 +1,31 @@
 package events
 
-type Event_t int32
-const (
-    DefaultEvent Event_t = iota
+import (
+    "github.com/gen2brain/raylib-go/raylib"
 )
+
+type Event_t int32
+
+const (
+    MoveUnits Event_t = iota
+    Attack
+)
+
+type Event_e interface {
+    unMarshall(string) MoveUnits_e
+}
+
+type MoveUnits_e struct {
+    Units   []string        `json:"Units"`
+    Dest    rl.Vector2      `json:"Dest"`
+}
+
 
 type Event struct {
     EventType  Event_t `json:"EventType"`
     Data       string `json:"Data"`
+}
+
+func EventUnmarshal(v interface{}) {
+
 }
