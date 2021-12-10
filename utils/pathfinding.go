741d5c60c7df2605e7bdd94196e30cb9e2fa703c
Gabriel Jeantet <gabriel.jeantetpro@gmail.com>
2021-12-10 01:01:00 +0100

    flowfield is working

--- utils/pathfinding.go
+++ utils/pathfinding.go
@@ -13,26 +13,26 @@ func PathFinding(mapp Map, endPos rl.Vector2, step int32) [][]rl.Vector2 {
 	map_width  := mapp.Width * TileSize
 	map_height := mapp.Height * TileSize
 
-	array_width  := map_width / step + 2
-	array_height := map_height / step + 2
+	array_width  := map_width / step + 1
+	array_height := map_height / step + 1
 
 	var cost_field = make([][]uint16, array_width)
-	var integration_field = make([][]uint16, array_width)
+	var integration_field = make([][]float32, array_width)
 	var flow_field = make([][]rl.Vector2, array_width)
 	for x, _ := range flow_field {
 		flow_field[x] = make([]rl.Vector2, array_height)
 		cost_field[x] = make([]uint16, array_height)
-		integration_field[x] = make([]uint16, array_height)
+		integration_field[x] = make([]float32, array_height)
 	}
-
 
 	for x, line := range cost_field {
 		for y, _ := range line {
-			if x == 0 || int32(x) == array_width -1 || y == 0 || int32(y) == array_height -1 {
+			cost_field[x][y] = 1
+			if (int32(x)+1)*step > map_width || (int32(y)+1)*step > map_height {
 				cost_field[x][y] = ^uint16(0)
 			} else {
-				x_s := int32(x-1) * step
-				y_s := int32(y-1) * step
+				x_s := int32(x) * step
+				y_s := int32(y) * step
 				map_x := x_s / TileSize
 				map_y := y_s / TileSize
 				rect := rl.NewRectangle(float32(x_s), float32(y_s), float32(step), float32(step))
@@ -42,33 +42,35 @@ func PathFinding(mapp Map, endPos rl.Vector2, step int32) [][]rl.Vector2 {
 				for _, v :=  range to_test {
 					matrix := rl.Vector2Add(curr_vect, v)
 
-					cost_field[x][y] = 1
 					if int32(matrix.X) < mapp.Width &&
                        int32(matrix.Y) < mapp.Height &&
                        mapp.Grid[int32(matrix.X)][int32(matrix.Y)].Tile_Type != None {
 						center := rl.Vector2Add(curr_vect, half_vect)
 						center = rl.Vector2Add(center, v)
 						center = rl.Vector2Scale(center, float32(TileSize))
-						if rl.CheckCollisionCircleRec(center, float32(TileSize)/2, rect) {
+						if rl.CheckCollisionCircleRec(center, float32(TileSize)/2-1, rect) {
 							cost_field[x][y] = ^uint16(0)
 						}
 					}
 				}
-			}
-		}
-	}
+            }
+        }
+    }
+	endPos = rl.Vector2Scale(endPos, 1/float32(step))
+	end_x := int32(endPos.X)
+	end_y := int32(endPos.Y)
+    if cost_field[end_x][end_y] == ^uint16(0) {
+        return nil
+    }
 	for x, line := range integration_field {
 		for y, _ := range line {
-			integration_field[x][y] = ^uint16(0)
+			integration_field[x][y] = float32(^uint16(0))
 		}
 	}
-	endPos = rl.Vector2Scale(endPos, 1/float32(step))
-	end_x := int32(endPos.Y) + 1
-	end_y := int32(endPos.Y) + 1
 
 	integration_field[end_x][end_y] = 0
 	stack := []Point{{X:end_x, Y:end_y}}
-	to_test := [8]Point{ {X: -1, Y: -1},{X: -1, Y: 0}, {X: -1, Y: 1},{X: 0, Y: -1}, {X: 0, Y: 1}, {X: 1, Y: -1}, {X: 1, Y:0}, {X: 1, Y: 1} } 
+	to_test := [8]Point{ {X: -1, Y: -1},{X: -1, Y: 0}, {X: -1, Y: 1},{X: 0, Y: -1}, {X: 0, Y: 1}, {X: 1, Y: -1}, {X: 1, Y:0}, {X: 1, Y: 1} }
 
 	for len(stack) > 0 {
 		p := stack[len(stack)-1]
@@ -82,8 +84,8 @@ func PathFinding(mapp Map, endPos rl.Vector2, step int32) [][]rl.Vector2 {
 			}
 
 			if cost_field[v_x][v_y] != ^uint16(0) {
-				c_p := integration_field[p.X][p.Y]
-				c_v := cost_field[v_x][v_y]
+                c_p := integration_field[p.X][p.Y]
+				c_v := float32(cost_field[v_x][v_y])*rl.Vector2Length(rl.Vector2{X: float32(voisin.X),Y:float32(voisin.Y)})
 
 				if c_p + c_v < integration_field[v_x][v_y] {
 					integration_field[v_x][v_y] = c_p + c_v
@@ -94,7 +96,12 @@ func PathFinding(mapp Map, endPos rl.Vector2, step int32) [][]rl.Vector2 {
 	}
 	for x, line := range flow_field {
 		for y, _ := range line {
-			min_cost := ^uint16(0)
+			min_cost := float32(^uint16(0))
+
+            if integration_field[x][y] == 0 || cost_field[x][y] == ^uint16(0) {
+                flow_field[x][y] = rl.Vector2{X: 0.0, Y: 0.0}
+                continue
+            }
 
 			for _, voisin := range to_test {
 				v_x := int32(x) + voisin.X
@@ -106,7 +113,7 @@ func PathFinding(mapp Map, endPos rl.Vector2, step int32) [][]rl.Vector2 {
 
 				if integration_field[v_x][v_y] < min_cost {
 					min_cost = integration_field[v_x][v_y]
-					flow_field[x][y] = rl.Vector2{float32(voisin.X), float32(voisin.Y)}
+                    flow_field[x][y] = rl.Vector2{X: float32(voisin.X), Y: float32(voisin.Y)}
 				}
 			}
 		}
